690e92f4eebf9a2ad78ca7dbb55a71afd04bac31
TiGauravD <76115082+TiGauravD@users.noreply.github.com>
2022-02-10 21:48:29 +0530

    second commit

---
+++ app/src/main/java/com/farfromcampus/jeemocktestseries/mocktestactivity.kt
@@ -0,0 +1,11 @@
+package com.farfromcampus.jeemocktestseries
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class mocktestactivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mocktestactivity)
+    }
+}
---
+++ app/src/main/java/com/farfromcampus/jeemocktestseries/testactivity.kt
@@ -0,0 +1,11 @@
+package com.farfromcampus.jeemocktestseries
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class testactivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_testactivity)
+    }
+}
